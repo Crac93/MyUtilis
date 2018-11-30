257e56232d2604c035a79a9a185ed3e6b3a0ae46
Avalos, Carlos <Carlos.Avalos@landisgyr.com>
2018-11-30 12:25:15 -0600

    Add Comunication With Emails An MSMQ

---
+++ MyUtilis/Comunication/Emails.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtilis.Comunication
+{
+    class Emails
+    {
+
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public string UserCredential { get; set; }
+        public string PasswordCredential { get; set; }
+
+        public bool DefaultCredentials { get; set; }
+
+        public string Host { get; set; }
+
+        public string From { get; set; }
+        public Emails()
+        {
+            To = "";
+            Subject = "";
+            Body = "";
+        }
+
+        public Emails(string to, string subject, string body)
+        {
+            To = to;
+            Subject = subject;
+            Body = body;
+        }
+
+
+        /// <summary>
+        /// Execute email method.
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="userCredential"></param>
+        /// <param name="passCredential"></param>
+        /// <param name="from"></param>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        /// <param name="toMails"></param>
+        ///  <param name="defaultCredentials"></param>
+        public void Send(string host = "", string userCredential = "", string passCredential = "", string from = "", string subject = ""
+            , string body = "", string toMails = "",bool defaultCredentials = false)
+        {
+            if (host == "")
+                host = Host;
+            if (userCredential == "")
+                userCredential = UserCredential;
+            if (passCredential == "")
+                passCredential = PasswordCredential;
+            if (from == "")
+                from = From;
+            if (subject == "")
+                subject = Subject;
+            if (body == "")
+                body = Body;
+            if (toMails == "")
+                toMails = To;
+
+            try
+            {
+                SmtpClient SmtpServer = new SmtpClient();
+                MailMessage mail = new MailMessage();
+                SmtpServer.Credentials = new System.Net.NetworkCredential(userCredential, passCredential);
+                SmtpServer.UseDefaultCredentials = defaultCredentials;
+                SmtpServer.Host = host;
+                mail = new MailMessage();
+                mail.From = new MailAddress(from);
+                string[] _cc = toMails.Split(';');
+                if (To != null)
+                {
+                    foreach (string cc in _cc)
+                    {
+                        mail.To.Add(cc);
+                    }
+                }
+                mail.Subject = subject;
+                mail.Body = body;
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ MyUtilis/Comunication/MSMQ.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtilis.Comunication
+{
+    class MSMQ
+    {
+        const string MessagerPathDefault = @".\private$\QueueName";
+
+        public void SendMSMQ(string messageToSend, string messagerPath = MessagerPathDefault)
+        {
+            try
+            {
+                using (MessageQueue queue = new MessageQueue())
+                {
+                    queue.Path = messagerPath;
+                    if (!MessageQueue.Exists(queue.Path))
+                    {
+                        MessageQueue.Create(queue.Path);
+                    }
+                    var message = new System.Messaging.Message();
+                    message.Body = messageToSend;
+                    queue.Send(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public string ReceiveMSMQ(string messagerPath = MessagerPathDefault)
+        {
+            try
+            {
+                using (MessageQueue queue = new MessageQueue())
+                {
+                    queue.Path = messagerPath;
+                    var message = new System.Messaging.Message();
+                    message = queue.Receive(new TimeSpan(0, 0, 3));
+                    message.Formatter = new XmlMessageFormatter(new String[] { "System.String.mscorlib" });
+                    string msgReturn = message.Body.ToString();
+                    return msgReturn;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
+
